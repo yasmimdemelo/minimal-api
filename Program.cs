41fb2ef3f2989ccf721dd5570fd97ce8f4bf5448
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-10-15 08:03:48 -0300

    Feat: Crear la ruta /login y la clase LoginDTO después de app.Run

--- Program.cs
+++ Program.cs
@@ -3,4 +3,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "yasmimdemelo@gmail.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login con éxito");
+    else 
+        return Results.Unauthorized();
+});
+
 app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
