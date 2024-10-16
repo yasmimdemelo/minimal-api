5cda8cbe7130fef71fa52c4582db352adb13ce0f
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-10-16 06:14:19 -0300

    feat: add ConnectionStrings en appsettings y configurar DbContext en Program.cs

--- Infraestrutura/DB/DbContexto.cs
+++ Infraestrutura/DB/DbContexto.cs
@@ -5,13 +5,35 @@ namespace MinimalApi.Infraestrutura.Db;
 
 
 public class DbContexto : DbContext
+
 {
+    // Crear una injeccion de independencia para el objeto
+    private readonly IConfiguration _configuracaoAppSettings;
+    
+    // Hacer un constructor para receber una injeccion de independencia
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+       _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
     public DbSet<Administrador> Adiministradores { get; set;} = default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySql(
-            "string de conexão", 
-            ServerVersion.AutoDetect("string de conexão"));
+        // variable para usar el iten de la injeccion de independencia
+        // ? = Si no encuentra nada, regresa vacio
+        var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+
+        // Agregar validacion "if" de optionsBuilder, despues de configuracion en Program.CS
+        if(!optionsBuilder.IsConfigured)
+        {
+            // Configuracion que passamos en el construtor
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao, 
+                    ServerVersion.AutoDetect(stringConexao));
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Recuperar mi stringConexao despues de configurada em DbContexto
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
     if(loginDTO.Email == "yasmimdemelo@gmail.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login con éxito");
     else 
