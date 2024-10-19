c40353332e49ecf0e245630c074a41f747dc25b1
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-10-19 08:19:11 -0300

    feat: Crear una interfaz y heredar AdministradorServicio de ella

---
+++ Dominio/Interfaces/IAdministradorServico.cs
@@ -0,0 +1,6 @@
+namespace MinimalApi.Dominio.Interfaces;
+
+public class IAdministradorServico
+{
+    
+}
---
+++ Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,8 @@
+using MinimalApi.Dominio.Interfaces;
+
+namespace MinimalApi.Dominio.Servicos;
+
+public class AdministradorServico : IAdministradorServico
+{
+    
+}
