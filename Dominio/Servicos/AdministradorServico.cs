a7d5c0c55b50678735642dcae1b7a25adc13b7aa
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-10-19 08:55:40 -0300

    feat: Implementación inicial de la clase AdministradorServico con inyección de dependencias y estructura básica para el método Login

--- Dominio/Servicos/AdministradorServico.cs
+++ Dominio/Servicos/AdministradorServico.cs
@@ -1,8 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Interfaces;
+using MinimalApi.DTOs;
 
 namespace MinimalApi.Dominio.Servicos;
 
 public class AdministradorServico : IAdministradorServico
 {
-    
+    //  Crear una injeccion de independencia para el objeto
+    private readonly DbContext _contexto;
+
+    // Hacer un constructor para receber una injeccion de independencia
+    public AdministradorServico(DbContext contexto)
+    {
+        _contexto = contexto;
+    }
+
+    // Agregar el metodo de DTOs
+    public List<Administrador> Login(LoginDTO loginDTO)
+    {
+        throw new NotImplementedException();
+    }
 }
